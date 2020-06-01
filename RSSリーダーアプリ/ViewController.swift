a5f99808cb90fa684cafe9cb660047a665169804
kawada-y <kawada-y@illmatics.co.jp>
2020-06-01 15:48:10 +0900

    Initial Commit

---
+++ RSSリーダーアプリ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSリーダーアプリ
+//
+//  Created by kawada-y on 2020/06/01.
+//  Copyright © 2020 kawada-y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
